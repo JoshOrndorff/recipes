fbabdd6aedfa9e74e64a9cd407d722638c73f63e
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-07-11 01:35:49 +0200

    Clean up, add node template

--- kitchen/event/adder/src/lib.rs
+++ kitchen/event/adder/src/lib.rs
@@ -1,32 +1,35 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 /// Adding Machine
 /// A simple adding machine which checks for overflow and emits an event with
 /// the result, without using storage.
 
-use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
+use support::{decl_module, decl_event, dispatch::Result};
 use system::ensure_signed;
 
 pub trait Trait: system::Trait {
-    type Event: From<Event> + Into<<Self as system::Trait>::Event>;
+	type Event: From<Event> + Into<<Self as system::Trait>::Event>;
 }
 
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        fn deposit_event() = default;
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		fn deposit_event() = default;
 
-        fn add(_origin, val1: u32, val2: u32) -> Result {
-            // checks for overflow
-            let result = match val1.checked_add(val2) {
-                Some(r) => r,
-                None => return Err("Addition overflowed"),
-            };
-            Self::deposit_event(Event::Added(val1, val2, result));
-            Ok(())
-        }
-    }
+		fn add(origin, val1: u32, val2: u32) -> Result {
+			let _ = ensure_signed(origin)?;
+			// checks for overflow
+			let result = match val1.checked_add(val2) {
+				Some(r) => r,
+				None => return Err("Addition overflowed"),
+			};
+			Self::deposit_event(Event::Added(val1, val2, result));
+			Ok(())
+		}
+	}
 }
 
 decl_event!(
-    pub enum Event {
-        Added(u32, u32, u32),
-    }
+	pub enum Event {
+		Added(u32, u32, u32),
+	}
 );
--- kitchen/storage/value/src/lib.rs
+++ kitchen/storage/value/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 /// Single Value Storage
 
 use support::{ensure, decl_module, decl_storage, StorageValue, dispatch::Result};
@@ -7,17 +9,17 @@ pub trait Trait: system::Trait {}
 
 decl_storage! {
 	trait Store for Module<T: Trait> as SingleValue {
-        MyValue: u32;
+		MyValue: u32;
 	}
 }
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		fn set_value(origin, value: u32) -> Result {
-            // check sender signature to verify permissions
-            let sender = ensure_signed(origin)?; 
-            <MyValue<T>>::put(value);
-            Ok(())
-        }
+			// check sender signature to verify permissions
+			let sender = ensure_signed(origin)?; 
+			<MyValue<T>>::put(value);
+			Ok(())
+		}
 	}
 }
