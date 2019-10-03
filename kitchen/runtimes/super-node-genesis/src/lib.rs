f9c175ae5ab4eaef040ad9e3ff110726292124eb
Joshy Orndorff <admin@joshyorndorff.com>
2019-10-02 17:49:18 -0800

    One crate per config. In hopes of combining configs with runtimes later.

--- kitchen/runtimes/super-node-genesis/src/lib.rs
+++ kitchen/runtimes/super-node-genesis/src/lib.rs
@@ -5,7 +5,7 @@ use super_node_runtime::{
 use babe_primitives::{AuthorityId as BabeId};
 use grandpa_primitives::{AuthorityId as GrandpaId};
 
-pub fn super_node_genesis(initial_authorities: Vec<(AccountId, AccountId, GrandpaId, BabeId)>,
+pub fn testnet_genesis(initial_authorities: Vec<(AccountId, AccountId, GrandpaId, BabeId)>,
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool) -> GenesisConfig {
