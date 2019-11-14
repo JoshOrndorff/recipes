784cba5cde68ff30699496bd8379f93a58806772
4meta5 <asinghchrony@protonmail.com>
2019-11-14 16:03:07 +0100

    update hello substrate runtime logic

--- kitchen/modules/hello-substrate/src/lib.rs
+++ kitchen/modules/hello-substrate/src/lib.rs
@@ -1,5 +1,8 @@
 /// A very simple substrate runtime
-use support::{decl_module, decl_event, decl_storage, StorageValue, StorageMap};
+use support::{
+	decl_module, decl_event, decl_storage, StorageValue, StorageMap,
+	dispatch::Result, ensure
+};
 use system::ensure_signed;
 
 pub trait Trait: system::Trait {
@@ -11,6 +14,8 @@ decl_event!{
 		AccountId = <T as system::Trait>::AccountId,
 	{
 		ValueSet(AccountId, u64),
+		ValueGot(AccountId, u64),
+		UserValueGot(AccountId, u64),
 	}
 }
 
@@ -25,11 +30,33 @@ decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		fn deposit_event() = default;
 
-		pub fn set_value(origin, value: u64) {
-			let sender = ensure_signed(origin)?;
+		pub fn set_value(origin, value: u64) -> Result {
+			let setter = ensure_signed(origin)?;
 			LastValue::put(value);
-			UserValue::<T>::insert(&sender, value);
-			Self::deposit_event(RawEvent::ValueSet(sender, value));
+			UserValue::<T>::insert(&setter, value);
+			Self::deposit_event(RawEvent::ValueSet(setter, value));
+			Ok(())
+		}
+
+		pub fn get_last_value(origin) -> Result {
+			let getter = ensure_signed(origin)?;
+			ensure!(LastValue::exists(), "no value stored in LastValue storage item");
+			let value = LastValue::get();
+			Self::deposit_event(RawEvent::ValueGot(getter, value));
+			Ok(())
+		}
+
+		pub fn get_user_value(origin) -> Result {
+			let getter = ensure_signed(origin)?;
+			ensure!(UserValue::<T>::exists(&getter), "value for user's AccountId does not exist");
+			let value = UserValue::<T>::get(&getter);
+			Self::deposit_event(RawEvent::ValueGot(getter, value));
+			Ok(())
 		}
 	}
+}
+
+#[cfg(test)]
+mod tests {
+	// TODO: add mock runtime here
 }
