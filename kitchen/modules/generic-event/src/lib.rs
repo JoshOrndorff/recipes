ee42effa81056e11875ff3149c58233384877375
4meta5 <asinghchrony@protonmail.com>
2019-11-17 17:27:27 +0100

    test generic event

--- kitchen/modules/generic-event/src/lib.rs
+++ kitchen/modules/generic-event/src/lib.rs
@@ -30,3 +30,88 @@ decl_event!(
         EmitInput(AccountId, u32),
     }
 );
+
+#[cfg(test)]
+mod tests {
+	use support::{impl_outer_origin, impl_outer_event, parameter_types};
+	use runtime_primitives::{Perbill, traits::{IdentityLookup, BlakeTwo256}, testing::Header};
+    use system::{EventRecord, Phase};
+    use super::RawEvent;
+    use runtime_io;
+	use primitives::H256;
+    use crate::{Module, Trait};
+
+    impl_outer_origin!{
+		pub enum Origin for Runtime {}
+	}
+
+	// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+	#[derive(Clone, PartialEq, Eq, Debug)]
+	pub struct Runtime;
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+	impl system::Trait for Runtime {
+		type Origin = Origin;
+		type Index = u64;
+		type Call = ();
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = TestEvent;
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+    }
+
+    mod generic_event {
+        pub use super::super::*;
+    }
+    
+    impl_outer_event! {
+        pub enum TestEvent for Runtime {
+            generic_event<T>,
+        }
+    }
+
+	impl Trait for Runtime {
+		type Event = TestEvent;
+    }
+
+	pub type System = system::Module<Runtime>;
+	pub type GenericEvent = Module<Runtime>;
+
+	pub struct ExtBuilder;
+
+	impl ExtBuilder {
+		pub fn build() -> runtime_io::TestExternalities {
+			let mut storage = system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+			runtime_io::TestExternalities::from(storage)
+		}
+    }
+
+    #[test]
+    fn test() {
+        ExtBuilder::build().execute_with(|| {
+            GenericEvent::do_something(Origin::signed(1), 32);
+
+            // construct event that should be emitted in the method call directly above
+            use system::ensure_signed;
+            let caller_id = ensure_signed(Origin::signed(1)).unwrap();
+            let expected_event = TestEvent::generic_event(
+				RawEvent::EmitInput(caller_id, 32),
+            );
+            
+			// iterate through array of `EventRecord`s
+			assert!(System::events().iter().any(|a| a.event == expected_event));
+        })
+    }
+}
--- kitchen/modules/single-value/src/lib.rs
+++ kitchen/modules/single-value/src/lib.rs
@@ -71,3 +71,82 @@ decl_module! {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+	use support::{impl_outer_origin, impl_outer_event, parameter_types, traits::Get};
+	use runtime_primitives::{Perbill, traits::{IdentityLookup, BlakeTwo256}, testing::Header};
+    use system::{EventRecord, Phase};
+    use runtime_io;
+	use primitives::H256;
+	use crate::{Module, Trait};
+
+	impl_outer_origin!{
+		pub enum Origin for Runtime {}
+	}
+
+	// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+	#[derive(Clone, PartialEq, Eq, Debug)]
+	pub struct Runtime;
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+	impl system::Trait for Runtime {
+		type Origin = Origin;
+		type Index = u64;
+		type Call = ();
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = TestEvent;
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+    }
+
+    mod singlevalue {
+        pub use crate::Event;
+    }
+    
+    impl_outer_event! {
+        pub enum TestEvent for Runtime {
+            singlevalue<T>,
+        }
+    }
+
+	impl Trait for Runtime {
+		type Event = TestEvent;
+    }
+
+	pub type System = system::Module<Runtime>;
+	pub type SingleValue = Module<Runtime>;
+
+	pub struct ExtBuilder;
+
+	impl ExtBuilder {
+		pub fn build() -> runtime_io::TestExternalities {
+			let mut storage = system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+			runtime_io::TestExternalities::from(storage)
+		}
+    }
+
+    #[test]
+    fn set_value_works() {
+        ExtBuilder::build().execute_with(|| {
+            assert!(true);
+        })
+    }
+
+    #[test]
+    fn get_value_works() {
+        assert!(true);
+    }
+}
