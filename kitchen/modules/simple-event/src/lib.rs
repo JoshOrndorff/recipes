024c9b5da1874db490f732f9130bc7063ce3b407
4meta5 <asinghchrony@protonmail.com>
2019-11-17 16:44:42 +0100

    simple event

--- kitchen/modules/simple-event/src/lib.rs
+++ kitchen/modules/simple-event/src/lib.rs
@@ -31,3 +31,90 @@ decl_event!(
         EmitInput(u32),
     }
 );
+
+
+#[cfg(test)]
+mod tests {
+	use support::{impl_outer_origin, impl_outer_event, parameter_types, traits::Get};
+	use runtime_primitives::{Perbill, traits::{IdentityLookup, BlakeTwo256}, testing::Header};
+    use system::{EventRecord, Phase};
+    use runtime_io;
+	use primitives::H256;
+    use crate::{Module, Trait};
+
+    impl_outer_origin!{
+		pub enum Origin for Runtime {}
+	}
+
+	// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+	#[derive(Clone, PartialEq, Eq, Debug)]
+	pub struct Runtime;
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+	impl system::Trait for Runtime {
+		type Origin = Origin;
+		type Index = u64;
+		type Call = ();
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = TestEvent;
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+    }
+
+    mod simple_event {
+        pub use crate::Event;
+    }
+    
+    impl_outer_event! {
+        pub enum TestEvent for Runtime {
+            simple_event,
+        }
+    }
+
+	impl Trait for Runtime {
+		type Event = TestEvent;
+    }
+
+	pub type System = system::Module<Runtime>;
+	pub type SimpleEvent = Module<Runtime>;
+
+	pub struct ExtBuilder;
+
+	impl ExtBuilder {
+		pub fn build() -> runtime_io::TestExternalities {
+			let mut storage = system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+			runtime_io::TestExternalities::from(storage)
+		}
+    }
+
+    #[test]
+    fn test() {
+        ExtBuilder::build().execute_with(|| {
+            SimpleEvent::do_something(Origin::signed(1), 32);
+
+            assert_eq!(
+                System::events(),
+                vec![
+                    EventRecord {
+                        phase: Phase::ApplyExtrinsic(0),
+                        event: TestEvent::simple_event(crate::Event::EmitInput(32)),
+                        topics: vec![],
+                    }
+                ]
+            );
+        })
+    }
+
+}
