e175d9ea101c985ef2a2423b19a57dd1d2a8d6f1
4meta5 <asinghchrony@protonmail.com>
2019-11-17 19:12:50 +0100

    run cargo fmt on a few modules, add test for simple-map

--- kitchen/modules/last-caller/src/lib.rs
+++ kitchen/modules/last-caller/src/lib.rs
@@ -1,13 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 /// An example instantiable module (without default instance)
-use support::{decl_module, decl_event, decl_storage, dispatch::Result, StorageValue};
+use support::{decl_event, decl_module, decl_storage, dispatch::Result, StorageValue};
 use system::{self, ensure_signed};
 
 // The module's configuration trait takes an instance as a type parameter. The instance type is
 // created by the `decl_storage!` macro below.
 pub trait Trait<I: Instance>: system::Trait {
-
     // The ubiquitous event type's From bound needs updated to support the instance.
     type Event: From<Event<Self, I>> + Into<<Self as system::Trait>::Event>;
 }
@@ -26,7 +25,10 @@ decl_storage! {
 
 decl_event!(
     // The enum trait also takes the Instance as a parameter
-    pub enum Event<T, I> where AccountId = <T as system::Trait>::AccountId {
+    pub enum Event<T, I>
+    where
+        AccountId = <T as system::Trait>::AccountId,
+    {
         Called(AccountId),
     }
 );
--- kitchen/modules/simple-event/src/lib.rs
+++ kitchen/modules/simple-event/src/lib.rs
@@ -32,71 +32,76 @@ decl_event!(
     }
 );
 
-
 #[cfg(test)]
 mod tests {
-	use support::{impl_outer_origin, impl_outer_event, parameter_types, traits::Get};
-	use runtime_primitives::{Perbill, traits::{IdentityLookup, BlakeTwo256}, testing::Header};
-    use system::{EventRecord, Phase};
-    use runtime_io;
-	use primitives::H256;
     use crate::{Module, Trait};
+    use primitives::H256;
+    use runtime_io;
+    use runtime_primitives::{
+        testing::Header,
+        traits::{BlakeTwo256, IdentityLookup},
+        Perbill,
+    };
+    use support::{impl_outer_event, impl_outer_origin, parameter_types, traits::Get};
+    use system::{EventRecord, Phase};
 
-    impl_outer_origin!{
-		pub enum Origin for Runtime {}
-	}
+    impl_outer_origin! {
+        pub enum Origin for Runtime {}
+    }
 
-	// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-	#[derive(Clone, PartialEq, Eq, Debug)]
-	pub struct Runtime;
-	parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: u32 = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::one();
-	}
-	impl system::Trait for Runtime {
-		type Origin = Origin;
-		type Index = u64;
-		type Call = ();
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = TestEvent;
-		type BlockHashCount = BlockHashCount;
-		type MaximumBlockWeight = MaximumBlockWeight;
-		type MaximumBlockLength = MaximumBlockLength;
-		type AvailableBlockRatio = AvailableBlockRatio;
-		type Version = ();
+    // Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+    #[derive(Clone, PartialEq, Eq, Debug)]
+    pub struct Runtime;
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: u32 = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::one();
+    }
+    impl system::Trait for Runtime {
+        type Origin = Origin;
+        type Index = u64;
+        type Call = ();
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = TestEvent;
+        type BlockHashCount = BlockHashCount;
+        type MaximumBlockWeight = MaximumBlockWeight;
+        type MaximumBlockLength = MaximumBlockLength;
+        type AvailableBlockRatio = AvailableBlockRatio;
+        type Version = ();
     }
 
     mod simple_event {
         pub use crate::Event;
     }
-    
+
     impl_outer_event! {
         pub enum TestEvent for Runtime {
             simple_event,
         }
     }
 
-	impl Trait for Runtime {
-		type Event = TestEvent;
+    impl Trait for Runtime {
+        type Event = TestEvent;
     }
 
-	pub type System = system::Module<Runtime>;
-	pub type SimpleEvent = Module<Runtime>;
+    pub type System = system::Module<Runtime>;
+    pub type SimpleEvent = Module<Runtime>;
 
-	pub struct ExtBuilder;
+    pub struct ExtBuilder;
 
-	impl ExtBuilder {
-		pub fn build() -> runtime_io::TestExternalities {
-			let mut storage = system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
-			runtime_io::TestExternalities::from(storage)
-		}
+    impl ExtBuilder {
+        pub fn build() -> runtime_io::TestExternalities {
+            let mut storage = system::GenesisConfig::default()
+                .build_storage::<Runtime>()
+                .unwrap();
+            runtime_io::TestExternalities::from(storage)
+        }
     }
 
     #[test]
@@ -106,13 +111,11 @@ mod tests {
 
             assert_eq!(
                 System::events(),
-                vec![
-                    EventRecord {
-                        phase: Phase::ApplyExtrinsic(0),
-                        event: TestEvent::simple_event(crate::Event::EmitInput(32)),
-                        topics: vec![],
-                    }
-                ]
+                vec![EventRecord {
+                    phase: Phase::ApplyExtrinsic(0),
+                    event: TestEvent::simple_event(crate::Event::EmitInput(32)),
+                    topics: vec![],
+                }]
             );
         })
     }
