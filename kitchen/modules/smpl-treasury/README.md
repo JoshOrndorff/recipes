60c7f4b18ec0da2e3da5183b8daf63686d128cde
4meta5 <asinghchrony@protonmail.com>
2019-11-21 19:39:31 +0100

    update smpl-treasury with more logic

--- kitchen/modules/smpl-treasury/README.md
+++ kitchen/modules/smpl-treasury/README.md
@@ -1,97 +1,157 @@
-# smpl-treasury
+# treasury module
 
-- [ ] reconfigure `node` and `runtime` with this module `Trait`
-- [ ] address node build issue
-
-* [Instantiate a Pot](#pot)
-* [Spend Requests](#proxytransfers) (needs to be updated)
-* [Schedule Spending](#schedule)    (needs to be updated)
-
-## Instantiate a Pot <a name = "pot"></a>
-
-To instantiate a pool of funds, import [`ModuleId`](https://crates.parity.io/sr_primitives/struct.ModuleId.html) and [`AccountIdConversion`](https://crates.parity.io/sr_primitives/traits/trait.AccountIdConversion.html) from [`sr-primitives`](https://crates.parity.io/sr_primitives/index.html).
+This `smpl-treasury` started as a simpler version of the `treasury` module, but it has since veered from the treasury's design. While it demonstrates similar patterns, the `smpl-treasury` defines a module in which the users that would like to transfer funds must call the runtime method `request_transfer`:
 
 ```rust
-use runtime_primitives::{ModuleId, traits::AccountIdConversion};
+// in decl_module {}
+fn request_transfer(
+    origin, 
+    dest: T::AccountId, 
+    amount: BalanceOf<T>
+) -> Result {
+    let sender = ensure_signed(origin)?;
+
+    let bond = T::Tax::get();
+    T::Currency::reserve(&sender, bond)
+        .map_err(|_| "Must be able to pay tax to make transfer")?;
+    
+    let requested_spend = SpendRequest {
+        from: sender.clone(),
+        to: dest.clone(),
+        amount: amount.clone(),
+    };
+    <TransferRequests<T>>::append(&[requested_spend])?;
+    Self::deposit_event(RawEvent::TransferRequested(sender, dest, amount));
+    Ok(())
+}
 ```
 
-With these imports, a `MODULE_ID` constant can be generated as an identifier for the pool of funds. This identifier can be converted into an `AccountId` with the `into_account()` method provided by the [`AccountIdConversion`](https://crates.parity.io/sr_primitives/traits/trait.AccountIdConversion.html) trait.
+This method reserves collateral from the sender before appending the `SpendRequest` to a runtime storage value.
+
+```rust
+// in decl_storage
+TransferRequests get(fn treasury_requests): Vec<SpendRequest<T::AccountId, BalanceOf<T>>>;
+```
+
+This vector of spend requests contains the fields `from`, `to`, `amount`.
+
+```rust
+#[derive(Encode, Decode)]
+pub struct SpendRequest<AccountId, Balance> {
+    /// Sending account
+    from: AccountId,
+    /// Receiving account
+    to: AccountId,
+    /// Send amount
+    amount: Balance,
+}
+```
+
+In essence, this runtime queues requests before batching execution every `UserSpend` number of blocks. `UserSpend` is a [configurable module constant](https://substrate.dev/recipes/storage/constants.html). It is used in [`on_finalize`](https://github.com/substrate-developer-hub/recipes/blob/master/src/tour/loop.md)
+
+```rust
+fn on_finalize(n: T::BlockNumber) {
+    if (n % T::UserSpend::get()).is_zero() {
+        // every `UserSpend` number of blocks,
+        // spend the funds according to member preferences
+        Self::user_spend();
+    }
+
+    if (n % T::TreasurySpend::get()).is_zero() {
+        Self::treasury_spend();
+    }
+
+}
+```
+
+If you looked closely in the `request_transfer` runtime method, a uniform fee called `T::Tax::get()` is taken from the sender for every transfer. It is reserved using `reserve` when the request is initially queued.
+
+```rust
+// in decl_module::request_transfer
+let bond = T::Tax::get();
+T::Currency::reserve(&sender, bond)
+    .map_err(|_| "Must be able to pay tax to make transfer")?;
+```
+
+It is transferred to the module's pot when the transfer is executed.
+
+```rust
+let _ = T::Currency::transfer(&request.from, &request.to, request.amount, AllowDeath);
+// get the tax
+let tax_to_pay = T::Tax::get();
+// unreserve the tax from the sender
+T::Currency::unreserve(&request.from, tax_to_pay);
+// pay the associated tax from the sender to the treasury account
+let _ = T::Currency::transfer(&request.from, &Self::account_id(), tax_to_pay, AllowDeath);
+```
+
+This example demonstrates one pattern for expressing a shared account with substrate. The `ModuleId` is a constant.
 
 ```rust
 const MODULE_ID: ModuleId = ModuleId(*b"example ");
+```
 
-impl<T: Trait> Module<T> {
-    pub fn account_id() -> T::AccountId {
-		MODULE_ID.into_account()
-	}
+This type comes from `sr-primitives`, which our `Cargo.toml` aliases as `runtime_primitives` so the import is
 
-    fn pot() -> BalanceOf<T> {
-		T::Currency::free_balance(&Self::account_id())
-	}
+```rust
+use runtime_primitives::ModuleId;
+```
+
+To convert this identity into an `AccountId`, it is necessary to also import `AccountIdConversion` from `sr-primitives::traits::AccountIdConversion`.
+
+```rust
+use runtime_primitives::traits::AccountIdConversion;
+```
+
+This is used to access the `ModuleId` associated with the module-driven governance with the `into_account` method
+
+```rust
+// in impl<T: Trait> Module<T>
+pub fn account_id() -> T::AccountId {
+    MODULE_ID.into_account()
 }
 ```
 
-Accessing the pot's balance is as simple as using the [`Currency`](https://crates.parity.io/srml_support/traits/trait.Currency.html) trait to access the balance of the associated `AccountId`.
+In our module, a uniform `Tax` is sent to the `ModuleId` for every `SpendRequest` executed. With `ModuleId` identifier, it is straightforward to define runtime logic to govern scheduled spending from this account as well.
 
-## Spend Requests <a name = "proxytransfers"></a>
+In our example, there is a `Council` storage item which is a list of `AccountId`s that are permitted to propose `Proposal`s for the treasury's spending. 
 
-In [srml/treasury](https://github.com/paritytech/substrate/blob/master/srml/treasury/src/lib.rs), approved spending proposals are queued in runtime storage before they are scheduled for execution. For the example dispatch queue, each entry represents a request to transfer `BalanceOf<T>` to `T::AccountId` from the pot.
+**In its current state, this runtime is terrifyingly insecure, but we can learn a lot by improving it, one step at a time.**
 
-```rust
-decl_storage! {
-	trait Store for Module<T: Trait> as STreasury {
-		/// the amount, the address to which it is sent
-		SpendQ get(fn spend_q): Vec<(T::AccountId, BalanceOf<T>)>;
-	}
-}
-```
-
-In other words, the dispatch queue holds the `AccountId` of the recipient (destination) in the first field of the tuple and the `BalanceOf<T>` in the second field. The runtime method for adding a spend request to the queue looks like this
+It allows every member to submit `Proposal`s for the treasury's spending. To vote on `Proposal`s, members need to call the `stupid_vote` runtime method with the `AccountId` of the member that made the `Proposal`. 
 
 ```rust
-decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // uses the example treasury as a proxy for transferring funds
-        fn proxy_transfer(origin, dest: T::AccountId, amount: BalanceOf<T>) -> Result {
-            let sender = ensure_signed(origin)?;
-
-            let _ = T::Currency::transfer(&sender, &Self::account_id(), amount)?;
-            <SpendQ<T>>::mutate(|requests| requests.push((dest.clone(), amount)));
-            Self::deposit_event(RawEvent::ProxyTransfer(dest, amount));
-            Ok(())
-        }
+fn stupid_vote(
+    origin,
+    vote: T::AccountId,
+) -> Result {
+    let voter = ensure_signed(origin)?;
+    ensure!(Self::is_on_council(&voter), "the voter is on the council");
+    if let Some(mut proposal) = <Proposals<T>>::get(vote) {
+        proposal.support += 1;
+    } else {
+        return Err("proposal associated with vote does not exist")
     }
+    Ok(())
 }
 ```
 
-This method transfers some funds to the pot along with the request to transfer the same funds from the pot to a recipient (the input field `dest: T::AccountId`).
-
-NOTE: *Instead of relying on direct requests, [srml/treasury](https://github.com/paritytech/substrate/blob/master/srml/treasury/src/lib.rs) coordinates spending decisions through a proposal process.*
-
-## Scheduling Spending <a name = "schedule"></a>
-
-To schedule spending like [`srml/treasury`](https://github.com/paritytech/substrate/blob/master/srml/treasury/src/lib.rs), first add a configurable module constant in the `Trait`. This constant determines how often the spending queue is executed.
+There is no limit on the number of times this method can be called by a member. So this voting algorithm is vulnerable to a sybil attack. It also doesn't check that the voter doesn't vote for themselves. Adding the latter check would only require placing another `ensure` statement at the top of the method body,
 
 ```rust
-pub trait Trait: system::Trait {
-    /// Period between successive spends.
-	type SpendPeriod: Get<Self::BlockNumber>;
-}
+ensure!(&voter != &vote, "cannot vote on their own proposal");
 ```
 
-This constant is invoked in the runtime method [`on_finalize`](https://crates.parity.io/sr_primitives/traits/trait.OnFinalize.html) to schedule spending every `T::SpendPeriod::get()` blocks.
+There is a lot of research on anti-sybil mechanisms. In this case, the most straightforward solution is probably to separate things out into a `ProposalPeriod` and `VotingPeriod` and store some shared state on-chain which indicates which period it is in and how much time is left until the next period. These windows should not overlap and member actions should be restricted within each period.
 
-```rust
-decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // other runtime methods
-        fn on_finalize(n: T::BlockNumber) {
-            if (n % T::SpendPeriod::get()).is_zero() {
-                Self::spend_funds();
-            }
-        }
-    }
-}
-```
+In `ProposalPeriod`, members should be able to make proposals. Each member should only be able to make one proposal. There should be a simple way of pointing at another member's proposal such that the pointer is invalidated (and there is a default) if the proposal (being pointed at) is changed.
 
-*To see the logic within `spend_funds`, see the [kitchen/treasury](https://github.com/substrate-developer-hub/recipes/tree/master/kitchen/treasury).* This recipe could be extended to give priority to certain spend requests or set a cap on the spends for a given `spend_funds()` call.
+During the transition between the `ProposalPeriod` and `VotePeriod`, it would be convenient if duplicate proposals or the pointing construct described above could be merged to minimize required on-chain state bloat and complexity.
+
+In `VotePeriod`, each member should be able to make only one vote. There should be a configurable role regarding whether or not a candidate can vote for their proposal. Each member should be able to change their vote as many times as requested before the end of the `VotePeriod`. 
+
+## Other Possible Extensions
+* design a system for electing a rotating council instead of the static vector of `AccountId`s; this would be a better model of how representative democracy is supposed to work for funding public infrastructure
+* prioritize certain proposals/requests based on increased `support` levels or something else (using `sort` with an `ord` implementation that might weight certain parameters a certain way)
+* implement quadratic signalling to separate the weighting algorithm from the voting process
+* swap about `Currency` with `GenericAsset` such that this system exists to control the transfer of a specific asset `=>` consider use cases in which this might be useful like ticket issuance according to some on-chain defined set of rules; maybe posting collateral like kickback but how should this organize in terms of objects and relationships
--- kitchen/modules/smpl-treasury/src/lib.rs
+++ kitchen/modules/smpl-treasury/src/lib.rs
@@ -1,124 +1,233 @@
-// Batching Balance Transfers through an Account with
-// automated, scheduled spending
-//
+// simple treasury
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use runtime_primitives::traits::{AccountIdConversion, Zero};
-use runtime_primitives::ModuleId;
-use support::traits::{Currency, Get, ReservableCurrency};
-use support::{decl_event, decl_module, decl_storage, dispatch::Result, StorageValue};
+use runtime_primitives::{ModuleId, RuntimeDebug};
+use support::traits::{Currency, ReservableCurrency, Get, ExistenceRequirement::AllowDeath};
+use support::{decl_event, decl_module, decl_storage, dispatch::Result, ensure, StorageValue};
 use parity_scale_codec::{Encode, Decode};
 use system::{self, ensure_signed};
+#[cfg(feature = "std")]
+use serde::{Serialize, Deserialize};
 use rstd::prelude::*;
 
 type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
-
 const MODULE_ID: ModuleId = ModuleId(*b"example ");
 
 pub trait Trait: system::Trait {
     /// The overarching event type.
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
-    /// The staking balance.
+    /// The currency type for this module
     type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
-    /// Minimum spend allowed for batched transfer
-    type MinimumSpend: Get<BalanceOf<Self>>;
-    /// The collateral required for each request (could change to Perbill::percent)
-    type RequestCollateral: Get<BalanceOf<Self>>;
-    /// Period between successive spends.
-    type SpendPeriod: Get<Self::BlockNumber>;
+    /// The collateral associated with a transfer
+    type Tax: Get<BalanceOf<Self>>;
+    /// Period between successive batch spends
+    type UserSpend: Get<Self::BlockNumber>;
+    /// Period between successive treasuery spends
+    type TreasurySpend: Get<Self::BlockNumber>;
+    /// Minimum amount of time until a proposal might get approved
+    type MinimumProposalAge: Get<Self::BlockNumber>;
 }
 
 #[derive(Encode, Decode)]
-pub struct SpendRequest<T::AccountId, BalanceOf> {
+pub struct SpendRequest<AccountId, Balance> {
+    /// Sending account
+    from: AccountId,
+    /// Receiving account
     to: AccountId,
+    /// Send amount
     amount: Balance,
 }
+
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct Proposal<AccountId, Balance, BlockNumber> {
+    /// Receiving Account
+    to: AccountId,
+    /// Expenditure amount
+    amount: Balance,
+    /// Submission blocknumber (for age requirement)
+    when: BlockNumber,
+    /// Simple support metric
+    support: u32,
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as SmplTreasury {
+        /// The amount, the address to which it is sent
+        TransferRequests get(fn treasury_requests): Vec<SpendRequest<T::AccountId, BalanceOf<T>>>;
+        /// The members which vote on how taxes are spent
+        Council get(fn council):  Vec<T::AccountId>;
+        /// The proposals that have yet to be approved for spending
+        Proposals get(fn proposals): map T::AccountId => Option<Proposal<T::AccountId, BalanceOf<T>, T::BlockNumber>>;
+    }
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        Balance = BalanceOf<T>,
+        <T as system::Trait>::AccountId,
+        <T as system::Trait>::BlockNumber
+    {
+        /// New spend request
+        TransferRequested(AccountId, AccountId, Balance),
+        /// Transfer request approved and delayed spend is executed
+        TransferExecuted(AccountId, Balance, BlockNumber),
+        /// Treasury spend proposed (proposed destination)
+        TreasuryProposal(AccountId, Balance),
+        /// Treasury spend executed
+        TreasurySpent(AccountId, Balance, BlockNumber),
+    }
+);
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
-        const MinimumSpend: BalanceOf<T> = T::MinimumSpend::get();
-        const RequestCollateral: BalanceOf<T> = T::RequestCollateral::get();
-        const SpendPeriod: T::BlockNumber = T::SpendPeriod::get();
+        const Tax: BalanceOf<T> = T::Tax::get();
+        const UserSpend: T::BlockNumber = T::UserSpend::get();
+        const TreasurySpend: T::BlockNumber = T::TreasurySpend::get();
+        const MinimumProposalAge: T::BlockNumber = T::MinimumProposalAge::get();
 
-        /// Request to Schedule Batch Transfer 
-        fn request_transfer(origin, dest: T::AccountId, amount: BalanceOf<T>) -> Result {
+        /// Transfer Request
+        ///
+        /// SpendRequest`s are queued in the `SpendQ` which is dispatched every `SpendPeriod`
+        /// The tax amount is reserved from sender and paid when executing the spend (in an atomic operation) 
+        fn request_transfer(
+            origin, 
+            dest: T::AccountId, 
+            amount: BalanceOf<T>
+        ) -> Result {
             let sender = ensure_signed(origin)?;
 
-            ensure!(amount >= T::MinimumSpend::get(), "spend must be at least MinimumSpend");
-
-            // if `value` > RequestCollateral {reserve RequestCollateral}
-            // else reserve `value`
-            let bond = Self::calculate_bond(value);
-			T::Currency::reserve(&sender, bond)
-				.map_err(|_| "Sender's balance too low")?;
-
+            let bond = T::Tax::get();
+            T::Currency::reserve(&sender, bond)
+                .map_err(|_| "Must be able to pay tax to make transfer")?;
+            
             let requested_spend = SpendRequest {
+                from: sender.clone(),
                 to: dest.clone(),
                 amount: amount.clone(),
             };
-            <SpendQ<T>>::append(&[requested_spend])?;
-            Self::deposit_event(RawEvent::TransferRequest(dest, amount));
+            <TransferRequests<T>>::append(&[requested_spend])?;
+            Self::deposit_event(RawEvent::TransferRequested(sender, dest, amount));
+            Ok(())
+        }
+
+        /// Propose Spend
+        ///
+        /// members can propose capital spending to addresses (from the pot)
+        /// (discovery and discussion of worthwhile projects/people would be off-chain)
+        fn propose_treasury_spend(
+            origin,
+            dest: T::AccountId,
+            amount: BalanceOf<T>,
+        ) -> Result {
+            let proposer = ensure_signed(origin)?;
+            ensure!(Self::is_on_council(&proposer), "must be on council to make proposal");
+
+            // <*could add a bond associated with proposals here*>
+
+            let proposed_expenditure = Proposal {
+                to: dest.clone(),
+                amount: amount.clone(),
+                when: <system::Module<T>>::block_number(),
+                support: 1u32,
+            };
+            // if previous proposal exists, it is overwritten
+            <Proposals<T>>::insert(&proposer, proposed_expenditure);
+
+            Self::deposit_event(RawEvent::TreasuryProposal(dest, amount));
+            Ok(())
+        }
+
+        /// Stupid Vote
+        ///
+        /// No anti-sybil mechanism for voters
+        /// 1 vote per call, but no limit on votes
+        fn stupid_vote(
+            origin,
+            vote: T::AccountId,
+        ) -> Result {
+            let voter = ensure_signed(origin)?;
+            ensure!(Self::is_on_council(&voter), "the voter is on the council");
+            if let Some(mut proposal) = <Proposals<T>>::get(vote) {
+                proposal.support += 1;
+            } else {
+                return Err("proposal associated with vote does not exist")
+            }
             Ok(())
         }
 
         fn on_finalize(n: T::BlockNumber) {
-            if (n % T::SpendPeriod::get()).is_zero() {
-                // could reorder and reprioritize spends here
-                Self::spend_funds();
+            if (n % T::UserSpend::get()).is_zero() {
+                // every `UserSpend` number of blocks,
+                // spend the funds according to member preferences
+                Self::user_spend();
             }
+
+            if (n % T::TreasurySpend::get()).is_zero() {
+                Self::treasury_spend();
+            }
+
         }
     }
 }
 
-decl_storage! {
-    trait Store for Module<T: Trait> as Treasury {
-        SpendQ get(fn spend_q): Vec<SpendRequest<T::AccountId, BalanceOf<T>>>;
-    }
-}
-
-decl_event!(
-	pub enum Event<T>
-	where
-		Balance = BalanceOf<T>,
-		<T as system::Trait>::AccountId
-	{
-		/// New spend request (from, to, amount)
-		TransferRequest(AccountId, AccountId, Balance),
-        /// New spend execution (from, to, amount)
-        SpendExecute(AccountId, AccountId, Balance),
-	}
-);
-
 impl<T: Trait> Module<T> {
-    // Add public immutables and private mutables.
+    /// Check that the signer is on the Treasury's Council
+    pub fn is_on_council(who: &T::AccountId) -> bool {
+        Self::council().contains(who)
+    }
 
-    /// The account ID of the treasury pot.
-    ///
-    /// This actually does computation. If you need to keep using it, then make sure you cache the
-    /// value and only call this once.
+    /// The account ID of the Treasury's Council
     pub fn account_id() -> T::AccountId {
         MODULE_ID.into_account()
     }
 
+    /// The balance of the Treasury's Council's pot of taxed funds
     fn pot() -> BalanceOf<T> {
         T::Currency::free_balance(&Self::account_id())
     }
 
-    fn calculate_bond(value: BalanceOf<T>) -> BalanceOf<T> {
-        // request collateral is set as x s.t. 0 < x =< `T::RequestCollateral` depending on 
-        T::RequestCollateral::get().min(value);
-    } 
+    /// The user spend method for executing transfers requested by users
+    fn user_spend() {
+        <TransferRequests<T>>::get().into_iter().for_each(|request| {
+            // execute the transfer request
+            let _ = T::Currency::transfer(&request.from, &request.to, request.amount, AllowDeath);
+            // get the tax
+            let tax_to_pay = T::Tax::get();
+            // unreserve the tax from the sender
+            T::Currency::unreserve(&request.from, tax_to_pay);
+            // pay the associated tax from the sender to the treasury account
+            let _ = T::Currency::transfer(&request.from, &Self::account_id(), tax_to_pay, AllowDeath);
+        });
+    }
 
-    fn spend_funds() {
+    /// The treasury pot spends according to proposals and votes
+    ///
+    /// too much iteration in the runtime?
+    fn treasury_spend() {
         let mut budget_remaining = Self::pot();
-        // TODO: take iteration out of runtime and place in offchain worker
-        <SpendQ<T>>::get().into_iter().for_each(|request| {
-            if request.1 <= budget_remaining {
-                budget_remaining -= request.1;
-                let _ = T::Currency::transfer(&Self::account_id(), &request.0, &request.1);
-                Self::deposit_event(RawEvent::SpendExecute(&request.0, &request.1));
+        // take a slice of proposals with a certain age
+        let required_age = T::MinimumProposalAge::get();
+        let now = <system::Module<T>>::block_number();
+        let mut old_enough_proposals = Vec::new();
+        <Council<T>>::get().into_iter().for_each(|member| {
+            if let Some(proposal) = <Proposals<T>>::get(member) {
+                if now - proposal.when > required_age.into() {
+                    old_enough_proposals.push(proposal);
+                }
             }
         });
+
+        // sort based on support and pay off as many as possible with the budget
+        old_enough_proposals.into_iter().for_each(|proposal| {
+            if proposal.amount <= budget_remaining {
+                budget_remaining -= proposal.amount;
+                let _ = T::Currency::transfer(&Self::account_id(), &proposal.to, proposal.amount, AllowDeath);
+            }
+        })
     }
 }
