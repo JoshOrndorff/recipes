121540d2fdd8302a7387075e7a8e8ad669eaf4c1
Joshy Orndorff <admin@joshyorndorff.com>
2020-01-31 18:16:40 -0500

    Everything compiles; just need type aggregation.

--- kitchen/nodes/kitchen-node/src/chain_spec.rs
+++ kitchen/nodes/kitchen-node/src/chain_spec.rs
@@ -1,9 +1,10 @@
-use primitives::{Pair, Public};
-use runtime::{AccountId, GenesisConfig};
-use runtime_genesis::testnet_genesis;
+use primitives::{Pair, Public, sr25519};
 use babe_primitives::{AuthorityId as BabeId};
 use sp_finality_grandpa::{AuthorityId as GrandpaId};
 use substrate_service;
+use sp_runtime::traits::{Verify, IdentifyAccount};
+use runtime::{AccountId, GenesisConfig, Signature};
+use runtime_genesis::testnet_genesis;
 
 // Note this is the URL for the telemetry server
 //const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -29,6 +30,15 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 		.public()
 }
 
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Helper function to generate an account ID from seed
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
+	AccountPublic: From<<TPublic::Pair as Pair>::Public>
+{
+	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
 /// Helper function to generate session key from seed
 pub fn get_authority_keys_from_seed(seed: &str) -> (BabeId, GrandpaId) {
 	(
@@ -47,12 +57,12 @@ impl Alternative {
 				|| testnet_genesis(vec![
 					get_authority_keys_from_seed("Alice"),
 				],
-				get_from_seed::<AccountId>("Alice"),
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				vec![
-					get_from_seed::<AccountId>("Alice"),
-					get_from_seed::<AccountId>("Bob"),
-					get_from_seed::<AccountId>("Alice//stash"),
-					get_from_seed::<AccountId>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
 				],
 				true),
 				vec![],
@@ -68,20 +78,20 @@ impl Alternative {
 					get_authority_keys_from_seed("Alice"),
 					get_authority_keys_from_seed("Bob"),
 				],
-				get_from_seed::<AccountId>("Alice"),
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				vec![
-					get_from_seed::<AccountId>("Alice"),
-					get_from_seed::<AccountId>("Bob"),
-					get_from_seed::<AccountId>("Charlie"),
-					get_from_seed::<AccountId>("Dave"),
-					get_from_seed::<AccountId>("Eve"),
-					get_from_seed::<AccountId>("Ferdie"),
-					get_from_seed::<AccountId>("Alice//stash"),
-					get_from_seed::<AccountId>("Bob//stash"),
-					get_from_seed::<AccountId>("Charlie//stash"),
-					get_from_seed::<AccountId>("Dave//stash"),
-					get_from_seed::<AccountId>("Eve//stash"),
-					get_from_seed::<AccountId>("Ferdie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
 				],
 				true),
 				vec![],
