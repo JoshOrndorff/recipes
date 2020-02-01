167f25bf43c1f21e02d78349a6c2078fdcdfd53d
Joshy Orndorff <admin@joshyorndorff.com>
2020-02-01 10:29:13 -0500

    Clean up merge that got f*cked by modules -> pallets rename

--- kitchen/runtimes/super-runtime/aggregate_types.js
+++ kitchen/runtimes/super-runtime/aggregate_types.js
@@ -1,36 +1,39 @@
-// Reads in the type definitions from all modules in the runtime, naively aggregates them, and writes them to disk.
+// Reads in the type definitions from all pallets in the runtime, naively aggregates them,
+// and writes them to disk.
+
 const fs = require('fs');
 
-// A list of all the installed recipe modules' folder names. Does not include system modules
+// A list of all the installed recipe pallets' folder names.
+// Does not include system pallets because Apps already supports them.
 // Redundant with construct_runtime!
-const modules = [
-  "simple-event",
-  "generic-event",
+const pallets = [
   "adding-machine",
-  "single-value",
-  "vec-set",
-  "storage-cache",
-  "simple-map",
-  "double-map",
-  "linked-map",
-  "struct-storage",
-  "module-constant-config",
   "basic-token",
   "check-membership",
-  "execution-schedule",
-  "smpl-treasury",
-  "last-caller",
+  "constant-config",
   "default-instance",
+  "double-map",
+  "execution-schedule",
+  "generic-event",
+  "last-caller",
+  "linked-map",
+  "simple-event",
+  "simple-map",
+  "simple-treasury",
+  "single-value",
+  "storage-cache",
+  "struct-storage",
+  "vec-set",
 ]
 
-// Loop through install modules aggregating types
+// Loop through all pallets aggregating types
 let finalTypes = {}
-let moduleTypes;
-for (let dirname of modules) {
-  let path = `../../modules/${dirname}/types.json`;
-  moduleTypes = JSON.parse(fs.readFileSync(path, 'utf8'));
-  finalTypes = {...finalTypes, ...moduleTypes};
+let palletTypes;
+for (let dirname of pallets) {
+  let path = `../../pallets/${dirname}/types.json`;
+  palletTypes = JSON.parse(fs.readFileSync(path, 'utf8'));
+  finalTypes = {...finalTypes, ...palletTypes};
 }
 
 // Write output to disk
-fs.writeFileSync("types.json", JSON.stringify(finalTypes), 'utf8');
+fs.writeFileSync("types.json", JSON.stringify(finalTypes, null, 2), 'utf8');
