65637a54c89e475844aad765d608fdb06179c9b3
Alexander Popiak <alexander.popiak@parity.io>
2020-03-17 19:09:49 +0100

    add ringbuffer queue to pallets

---
+++ pallets/ringbuffer-queue/src/lib.rs
@@ -0,0 +1,111 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Ringbuffer
+use codec::{Decode, Encode};
+use frame_support::{
+	decl_event, decl_module, decl_storage, dispatch::DispatchResult, ensure, StorageMap,
+};
+use frame_system::{self as system, ensure_signed};
+
+mod ringbuffer;
+
+use ringbuffer::{RingBufferTrait, RingBufferTransient};
+
+#[cfg(test)]
+mod tests;
+
+type Index = u8;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct ValueStruct {
+	integer: i32,
+	boolean: bool,
+}
+
+pub trait Trait: system::Trait {
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as RingBufferQueue {
+		BufferMap get(fn get_value): map hasher(twox_64_concat) Index => ValueStruct;
+		BufferRange get(fn range): (Index, Index);
+
+		AMap: map hasher(twox_64_concat) &str => u32;
+	}
+}
+
+decl_event!(
+	pub enum Event<T>
+	where
+		AccountId = <T as system::Trait>::AccountId,
+	{
+		Popped(ValueStruct),
+		DummyEvent(AccountId),
+	}
+);
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		fn deposit_event() = default;
+
+		fn add_to_queue(origin, integer: i32, boolean: bool) -> DispatchResult {
+			// only a user can push into the queue
+			let _user = ensure_signed(origin)?;
+
+			let mut queue = Self::queue_transient();
+			queue.push(ValueStruct{ integer, boolean });
+
+			Ok(())
+		}
+
+		fn add_multiple(origin, integers: Vec<i32>, boolean: bool) -> DispatchResult {
+			// only a user can push into the queue
+			let _user = ensure_signed(origin)?;
+
+			let mut queue = Self::queue_transient();
+			for integer in integers {
+				queue.push(ValueStruct{ integer, boolean });
+			}
+
+			Ok(())
+		}
+
+		fn pop_from_queue(origin) -> DispatchResult {
+			// only a user can pop from the queue
+			let _user = ensure_signed(origin)?;
+
+			let mut queue = Self::queue_transient();
+			if let Some(value) = queue.pop() {
+				Self::deposit_event(RawEvent::Popped(value));
+			}
+
+			Ok(())
+		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+	fn queue_transient() -> Box<
+		dyn RingBufferTrait<
+			ValueStruct,
+			Index,
+			Bounds = <Self as Store>::BufferRange,
+			Map = <Self as Store>::BufferMap,
+		>,
+	> {
+		Box::new(RingBufferTransient::<
+			ValueStruct,
+			<Self as Store>::BufferRange,
+			<Self as Store>::BufferMap,
+			dyn RingBufferTrait<
+				ValueStruct,
+				Index,
+				Bounds = <Self as Store>::BufferRange,
+				Map = <Self as Store>::BufferMap,
+			>,
+			Index,
+		>::new())
+	}
+}
---
+++ pallets/ringbuffer-queue/src/tests.rs
@@ -0,0 +1,84 @@
+use super::{RawEvent, ValueStruct};
+use crate::{Module, Trait};
+use sp_core::H256;
+use sp_io::TestExternalities;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+	Perbill,
+};
+use frame_support::{assert_ok, assert_err, impl_outer_event, impl_outer_origin, parameter_types};
+use frame_system as system;
+
+impl_outer_origin! {
+	pub enum Origin for TestRuntime {}
+}
+
+// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TestRuntime;
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: u32 = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+}
+impl system::Trait for TestRuntime {
+	type Origin = Origin;
+	type Index = u64;
+	type Call = ();
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = TestEvent;
+	type BlockHashCount = BlockHashCount;
+	type MaximumBlockWeight = MaximumBlockWeight;
+	type MaximumBlockLength = MaximumBlockLength;
+	type AvailableBlockRatio = AvailableBlockRatio;
+	type Version = ();
+	type ModuleToIndex = ();
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+}
+
+mod simple_map {
+	pub use crate::Event;
+}
+
+impl_outer_event! {
+	pub enum TestEvent for TestRuntime {
+		simple_map<T>,
+		system<T>,
+	}
+}
+
+impl Trait for TestRuntime {
+	type Event = TestEvent;
+}
+
+pub type System = system::Module<TestRuntime>;
+pub type RingBuffer = Module<TestRuntime>;
+
+pub struct ExtBuilder;
+
+impl ExtBuilder {
+	pub fn build() -> TestExternalities {
+		let storage = system::GenesisConfig::default()
+			.build_storage::<TestRuntime>()
+			.unwrap();
+		TestExternalities::from(storage)
+	}
+}
+
+#[test]
+fn add_to_queue_works() {
+	ExtBuilder::build().execute_with(|| {
+		assert_ok!(RingBuffer::add_to_queue(Origin::signed(1), 1, true));
+		assert_eq!(RingBuffer::get_value(0), ValueStruct{ integer: 1, boolean: true});
+		assert_eq!(RingBuffer::range(), (0, 1));
+	})
+}
