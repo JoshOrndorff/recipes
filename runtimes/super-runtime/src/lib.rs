fd43d31e66dd24ec70b8b4f50e68efdcf17766f3
Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
2020-03-28 04:22:33 -0400

    Ditch linked Maps (#199)
    * cherry pick 1f1788e and resolve conflicts
    
    * Fix table of contents

--- runtimes/super-runtime/src/lib.rs
+++ runtimes/super-runtime/src/lib.rs
@@ -309,10 +309,6 @@ impl last_caller::Trait<last_caller::Instance2> for Runtime {
 	type Event = Event;
 }
 
-impl linked_map::Trait for Runtime {
-	type Event = Event;
-}
-
 // For offchain_demo
 
 parameter_types! {
@@ -429,7 +425,6 @@ construct_runtime!(
 		GenericEvent: generic_event::{Module, Call, Event<T>},
 		LastCaller1: last_caller::<Instance1>::{Module, Call, Storage, Event<T>},
 		LastCaller2: last_caller::<Instance2>::{Module, Call, Storage, Event<T>},
-		LinkedMap: linked_map::{Module, Call, Storage, Event<T>},
 		OffchainDemo: offchain_demo::{Module, Call, Storage, Event<T>, ValidateUnsigned},
 		RingbufferQueue: ringbuffer_queue::{Module, Call, Storage, Event<T>},
 		RandomnessDemo: randomness::{Module, Call, Storage, Event},
