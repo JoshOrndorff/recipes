bc98c2087ed27f6cdb21062af57c188f994fa6ce
Jimmy Chu <jimmychu0807@gmail.com>
2020-04-03 12:59:13 +0800

    updating code to conform to current style

--- runtimes/ocw-runtime/src/genesis.rs
+++ runtimes/ocw-runtime/src/genesis.rs
@@ -14,20 +14,20 @@ pub fn testnet_genesis(initial_authorities: Vec<(BabeId, GrandpaId)>,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool) -> GenesisConfig {
 	GenesisConfig {
-		system: Some(SystemConfig {
+		frame_system: Some(SystemConfig {
 			code: WASM_BINARY.to_vec(),
 			changes_trie_config: Default::default(),
 		}),
-		balances: Some(BalancesConfig {
+		pallet_balances: Some(BalancesConfig {
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
-		sudo: Some(SudoConfig {
+		pallet_sudo: Some(SudoConfig {
 			key: root_key,
 		}),
-		babe: Some(BabeConfig {
+		pallet_babe: Some(BabeConfig {
 			authorities: initial_authorities.iter().map(|x| (x.0.clone(), 1)).collect(),
 		}),
-		grandpa: Some(GrandpaConfig {
+		pallet_grandpa: Some(GrandpaConfig {
 			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
 		}),
 	}
