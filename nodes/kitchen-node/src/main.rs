158ecd91f3947ea7b6569853c49d3b5e079d4668
Joshy Orndorff <admin@joshyorndorff.com>
2020-04-17 15:30:52 -0400

    Kitchen node compiles with weight-fee runtime. Apps not working :-/

--- nodes/kitchen-node/src/main.rs
+++ nodes/kitchen-node/src/main.rs
@@ -10,16 +10,5 @@ mod cli;
 mod command;
 
 fn main() -> sc_cli::Result<()> {
-	let version = sc_cli::VersionInfo {
-		name: "Kitchen Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "kitchen-node",
-		author: "Anonymous",
-		description: "Kitchen Node",
-		support_url: "support.anonymous.an",
-		copyright_start_year: 2019,
-	};
-
-	command::run(version)
+	command::run()
 }
