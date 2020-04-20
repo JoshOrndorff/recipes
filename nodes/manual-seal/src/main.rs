9fb3b75dcf87ac38ab6ae75c61d10acfa237d307
Joshy Orndorff <admin@joshyorndorff.com>
2020-04-20 17:23:49 -0400

    update code to alpha.6

--- nodes/manual-seal/src/main.rs
+++ nodes/manual-seal/src/main.rs
@@ -1,4 +1,4 @@
-//! Substrate Node Template CLI library.
+//! Manaul / Instant Seal CLI library.
 #![warn(missing_docs)]
 
 mod chain_spec;
@@ -8,16 +8,5 @@ mod cli;
 mod command;
 
 fn main() -> sc_cli::Result<()> {
-	let version = sc_cli::VersionInfo {
-		name: "Basic PoW Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "basic-pow",
-		author: "Anonymous",
-		description: "A node that demonstrates minimal proof of work consensus",
-		support_url: "support.anonymous.an",
-		copyright_start_year: 2017,
-	};
-
-	command::run(version)
+	command::run()
 }
