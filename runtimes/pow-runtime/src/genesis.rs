a3eb8b252ca5b623e0ffeb0cc85fb11a0a31443a
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-01 10:57:06 -0400

    restructure pow node and runtime

--- runtimes/pow-runtime/src/genesis.rs
+++ runtimes/pow-runtime/src/genesis.rs
@@ -2,8 +2,40 @@
 
 use super::{
 	AccountId, BalancesConfig, GenesisConfig,
-	SudoConfig, SystemConfig, WASM_BINARY,
+	SudoConfig, SystemConfig, WASM_BINARY, Signature,
 };
+use sp_core::{Pair, sr25519};
+use sp_runtime::traits::{Verify, IdentifyAccount};
+
+/// Helper function to generate a crypto pair from seed
+fn get_from_seed<TPair: Pair>(seed: &str) -> TPair::Public {
+	TPair::from_string(&format!("//{}", seed), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Helper function to generate an account ID from seed
+pub fn account_id_from_seed<TPair: Pair>(seed: &str) -> AccountId where
+	AccountPublic: From<TPair::Public>
+{
+	AccountPublic::from(get_from_seed::<TPair>(seed)).into_account()
+}
+
+pub fn dev_genesis() -> GenesisConfig {
+	testnet_genesis(
+		// Root Key
+		account_id_from_seed::<sr25519::Pair>("Alice"),
+		// Endowed Accounts
+		vec![
+			account_id_from_seed::<sr25519::Pair>("Alice"),
+			account_id_from_seed::<sr25519::Pair>("Bob"),
+			account_id_from_seed::<sr25519::Pair>("Alice//stash"),
+			account_id_from_seed::<sr25519::Pair>("Bob//stash"),
+		],
+	)
+}
 
 /// This helper function is unlike the testnet_genesis functions in other runtimes.
 /// It does not accept a list of initial authorities because there is no concept of
@@ -11,7 +43,7 @@ use super::{
 pub fn testnet_genesis(
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool) -> GenesisConfig {
+) -> GenesisConfig {
 	GenesisConfig {
 		system: Some(SystemConfig {
 			code: WASM_BINARY.to_vec(),
