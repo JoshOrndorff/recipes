b651113b635f8b612cf88c848f4fb91e61d9db56
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-01 09:39:01 -0400

    Restructure kithen node / super runtime

--- runtimes/super-runtime/src/genesis.rs
+++ runtimes/super-runtime/src/genesis.rs
@@ -9,11 +9,8 @@ use sp_consensus_babe::{AuthorityId as BabeId};
 use sp_finality_grandpa::{AuthorityId as GrandpaId};
 use sp_runtime::traits::{Verify, IdentifyAccount};
 
-/// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
-
 /// Helper function to generate a crypto pair from seed
-pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
 	TPublic::Pair::from_string(&format!("//{}", seed), None)
 		.expect("static values are valid; qed")
 		.public()
@@ -22,50 +19,44 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 type AccountPublic = <Signature as Verify>::Signer;
 
 /// Helper function to generate an account ID from seed
-pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
+pub fn account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic: From<<TPublic::Pair as Pair>::Public>
 {
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 /// Helper function to generate session key from seed
-pub fn get_authority_keys_from_seed(seed: &str) -> (BabeId, GrandpaId) {
+pub fn authority_keys_from_seed(seed: &str) -> (BabeId, GrandpaId) {
 	(
 		get_from_seed::<BabeId>(seed),
 		get_from_seed::<GrandpaId>(seed),
 	)
 }
 
-/// Build a Development ChainSpec
-pub fn dev_config() -> ChainSpec {
-	ChainSpec::from_genesis(
-		"Development",
-		"dev",
-		sc_service::ChainType::Development,
-		|| testnet_genesis(vec![
-			get_authority_keys_from_seed("Alice"),
-		],
-		get_account_id_from_seed::<sr25519::Public>("Alice"),
+pub fn dev_genesis() -> GenesisConfig {
+	testnet_genesis(
+		// Initial Authorities
 		vec![
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			get_account_id_from_seed::<sr25519::Public>("Bob"),
-			get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-			get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+			authority_keys_from_seed("Alice"),
 		],
-		true),
-		vec![],
-		None,
-		None,
-		None,
-		None
+		// Root Key
+		account_id_from_seed::<sr25519::Public>("Alice"),
+		// Endowed Accounts
+		vec![
+			account_id_from_seed::<sr25519::Public>("Alice"),
+			account_id_from_seed::<sr25519::Public>("Bob"),
+			account_id_from_seed::<sr25519::Public>("Alice//stash"),
+			account_id_from_seed::<sr25519::Public>("Bob//stash"),
+		],
 	)
 }
 
 /// Helper function to build a genesis configuration
-pub fn testnet_genesis(initial_authorities: Vec<(BabeId, GrandpaId)>,
+pub fn testnet_genesis(
+	initial_authorities: Vec<(BabeId, GrandpaId)>,
 	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool) -> GenesisConfig {
+	endowed_accounts: Vec<AccountId>
+) -> GenesisConfig {
 	GenesisConfig {
 		system: Some(SystemConfig {
 			code: WASM_BINARY.to_vec(),
