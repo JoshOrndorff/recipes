03593eccb12acdf35bf2b70678e0359aef5a9d37
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-01 20:17:41 -0400

    standardize super runtime TODO How to remove the session key stuff from the runtime?

--- runtimes/super-runtime/src/lib.rs
+++ runtimes/super-runtime/src/lib.rs
@@ -1,4 +1,6 @@
-//! A Super Runtime. This runtime demonstrates most the recipe pallets in a single super runtime.
+//! A Super Runtime.
+//!
+//! This runtime demonstrates most the recipe pallets in a single super runtime.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
@@ -27,8 +29,6 @@ use sp_runtime::traits::{
 };
 use frame_system as system;
 use sp_api::impl_runtime_apis;
-use babe::SameAuthoritiesForever;
-use grandpa::AuthorityList as GrandpaAuthorityList;
 
 #[cfg(feature = "std")]
 use sp_version::NativeVersion;
@@ -90,10 +90,7 @@ pub mod opaque {
 	pub type BlockId = generic::BlockId<Block>;
 
 	impl_opaque_keys! {
-		pub struct SessionKeys {
-			pub grandpa: Grandpa, //TODO is this order correct? I changed stuff in chainspec.
-			pub babe: Babe,
-		}
+		pub struct SessionKeys {}
 	}
 }
 
@@ -108,18 +105,6 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 };
 
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
-
-pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// These time units are defined in number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
-
-// Some BABE-specific stuff
-// 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
-pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
-pub const EPOCH_DURATION_IN_BLOCKS: u32 = 10 * MINUTES;
 
 /// The version infromation used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
@@ -182,28 +167,13 @@ impl system::Trait for Runtime {
 }
 
 parameter_types! {
-	pub const EpochDuration: u64 = EPOCH_DURATION_IN_BLOCKS as u64;
-	pub const ExpectedBlockTime: u64 = MILLISECS_PER_BLOCK;
-}
-
-impl babe::Trait for Runtime {
-	type EpochDuration = EpochDuration;
-	type ExpectedBlockTime = ExpectedBlockTime;
-	type EpochChangeTrigger = SameAuthoritiesForever;
-}
-
-impl grandpa::Trait for Runtime {
-	type Event = Event;
-}
-
-parameter_types! {
-	pub const MinimumPeriod: u64 = SLOT_DURATION / 2;
+	pub const MinimumPeriod: u64 = 1000;
 }
 
 impl timestamp::Trait for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
-	type OnTimestampSet = Babe;
+	type OnTimestampSet = ();
 	type MinimumPeriod = MinimumPeriod;
 }
 
@@ -328,7 +298,8 @@ impl ringbuffer_queue::Trait for Runtime {
 impl randomness::Trait for Runtime {
 	type Event = Event;
 	type CollectiveFlipRandomnessSource = RandomnessCollectiveFlip;
-	type BabeRandomnessSource = Babe;
+	//TODO Refactor this recipe to only take one randomness source like a normal pallet would.
+	type BabeRandomnessSource = RandomnessCollectiveFlip;
 }
 
 impl simple_event::Trait for Runtime {
@@ -363,8 +334,6 @@ construct_runtime!(
 	{
 		System: system::{Module, Call, Storage, Config, Event<T>},
 		Timestamp: timestamp::{Module, Call, Storage, Inherent},
-		Babe: babe::{Module, Call, Storage, Config, Inherent(Timestamp)},
-		Grandpa: grandpa::{Module, Call, Storage, Config, Event},
 		Balances: balances::{Module, Call, Storage, Config<T>, Event<T>},
 		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Call, Storage},
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
@@ -480,34 +449,6 @@ impl_runtime_apis! {
 	impl sp_offchain::OffchainWorkerApi<Block> for Runtime {
 		fn offchain_worker(header: &<Block as BlockT>::Header) {
 			Executive::offchain_worker(header)
-		}
-	}
-
-	impl sp_finality_grandpa::GrandpaApi<Block> for Runtime {
-		fn grandpa_authorities() -> GrandpaAuthorityList {
-			Grandpa::grandpa_authorities()
-		}
-	}
-
-	impl sp_consensus_babe::BabeApi<Block> for Runtime {
-		fn configuration() -> sp_consensus_babe::BabeConfiguration {
-			// The choice of `c` parameter (where `1 - c` represents the
-			// probability of a slot being empty), is done in accordance to the
-			// slot duration and expected target block time, for safely
-			// resisting network delays of maximum two seconds.
-			// <https://research.web3.foundation/en/latest/polkadot/BABE/Babe/#6-practical-results>
-			sp_consensus_babe::BabeConfiguration {
-				slot_duration: Babe::slot_duration(),
-				epoch_length: EpochDuration::get(),
-				c: PRIMARY_PROBABILITY,
-				genesis_authorities: Babe::authorities(),
-				randomness: Babe::randomness(),
-				secondary_slots: true,
-			}
-		}
-
-		fn current_epoch_start() -> sp_consensus_babe::SlotNumber {
-			Babe::current_epoch_start()
 		}
 	}
 
