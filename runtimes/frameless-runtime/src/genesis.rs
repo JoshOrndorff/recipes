a1106cfdaad16dca24a351dbfaf391f82f2cc21f
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-13 10:50:00 -0400

    Compiles with kitchen node

--- runtimes/frameless-runtime/src/genesis.rs
+++ runtimes/frameless-runtime/src/genesis.rs
@@ -3,12 +3,35 @@
 //! `basic-pow` node without having to hack that node.
 
 use super::{
-	AccountId, GenesisConfig,
+	AccountId, GenesisConfig, Signature,
 };
+
+use sp_core::Pair;
+use sp_runtime::traits::{Verify, IdentifyAccount};
+
+/// Helper function to generate a crypto pair from seed
+fn get_from_seed<TPair: Pair>(seed: &str) -> TPair::Public {
+	TPair::from_string(&format!("//{}", seed), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Helper function to generate an account ID from seed
+pub fn account_id_from_seed<TPair: Pair>(seed: &str) -> AccountId where
+	AccountPublic: From<TPair::Public>
+{
+	AccountPublic::from(get_from_seed::<TPair>(seed)).into_account()
+}
+
+pub fn dev_genesis() -> GenesisConfig {
+	GenesisConfig
+}
 
 pub fn testnet_genesis(
 	_root_key: AccountId,
 	_endowed_accounts: Vec<AccountId>,
-	_enable_println: bool) -> GenesisConfig {
+) -> GenesisConfig {
 		GenesisConfig
 }
