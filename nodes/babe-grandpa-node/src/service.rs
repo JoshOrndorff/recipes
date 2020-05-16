1708b11966ecf2cb9300da4d7f55444e804f0897
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-16 03:38:29 -0400

    Start linting babe grandpa node

--- nodes/babe-grandpa-node/src/service.rs
+++ nodes/babe-grandpa-node/src/service.rs
@@ -9,7 +9,6 @@ use sc_service::{error::{Error as ServiceError}, AbstractService, Configuration,
 use sp_inherents::InherentDataProviders;
 use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
-use sc_consensus_babe;
 use sc_finality_grandpa::{
 	self,
 	FinalityProofProvider as GrandpaFinalityProofProvider,
@@ -169,7 +168,7 @@ pub fn new_full(config: Configuration)
 			config: grandpa_config,
 			link: grandpa_link,
 			network: service.network(),
-			inherent_data_providers: inherent_data_providers.clone(),
+			inherent_data_providers,
 			telemetry_on_connect: Some(service.telemetry_on_connect_stream()),
 			voting_rule: sc_finality_grandpa::VotingRulesBuilder::default().build(),
 			prometheus_registry: service.prometheus_registry(),
@@ -206,7 +205,7 @@ pub fn new_light(config: Configuration)
 		.with_transaction_pool(|config, client, fetcher, prometheus_registry| {
 			let fetcher = fetcher
 				.ok_or_else(|| "Trying to start light transaction pool without active fetcher")?;
-			let pool_api = sc_transaction_pool::LightChainApi::new(client.clone(), fetcher.clone());
+			let pool_api = sc_transaction_pool::LightChainApi::new(client, fetcher);
 			let pool = sc_transaction_pool::BasicPool::with_revalidation_type(
 				config,
 				Arc::new(pool_api),
@@ -247,7 +246,7 @@ pub fn new_light(config: Configuration)
 				babe_block_import,
 				None,
 				Some(Box::new(finality_proof_import)),
-				client.clone(),
+				client,
 				inherent_data_providers.clone(),
 				spawn_task_handle,
 				registry,
