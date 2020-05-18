6f91ef9995e74589d6048e37c6d1d1432a3e2411
Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
2020-05-18 14:24:43 -0400

    Clean cargo.toml files (#254)
    * default dash features
    
    * dealias a few system vs fram-system imports

--- pallets/generic-event/src/lib.rs
+++ pallets/generic-event/src/lib.rs
@@ -3,7 +3,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{decl_event, decl_module, dispatch::DispatchResult};
-use system::ensure_signed;
+use frame_system::{self as system, ensure_signed};
 
 #[cfg(test)]
 mod tests;
