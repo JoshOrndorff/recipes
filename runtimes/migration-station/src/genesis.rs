4825acab5b5be720d51e31bc6912ebbab3320f54
Joshy Orndorff <admin@joshyorndorff.com>
2020-06-01 14:32:18 -0400

    initial code

---
+++ runtimes/migration-station/src/genesis.rs
@@ -0,0 +1,56 @@
+//! Helper module to build a genesis configuration for the super-runtime
+
+use super::{
+	AccountId, BalancesConfig, GenesisConfig, Signature, SudoConfig, SystemConfig, WASM_BINARY,
+};
+use sp_core::{sr25519, Pair};
+use sp_runtime::traits::{IdentifyAccount, Verify};
+
+/// Helper function to generate a crypto pair from seed
+fn get_from_seed<TPair: Pair>(seed: &str) -> TPair::Public {
+	TPair::from_string(&format!("//{}", seed), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Helper function to generate an account ID from seed
+pub fn account_id_from_seed<TPair: Pair>(seed: &str) -> AccountId
+where
+	AccountPublic: From<TPair::Public>,
+{
+	AccountPublic::from(get_from_seed::<TPair>(seed)).into_account()
+}
+
+pub fn dev_genesis() -> GenesisConfig {
+	testnet_genesis(
+		// Root Key
+		account_id_from_seed::<sr25519::Pair>("Alice"),
+		// Endowed Accounts
+		vec![
+			account_id_from_seed::<sr25519::Pair>("Alice"),
+			account_id_from_seed::<sr25519::Pair>("Bob"),
+			account_id_from_seed::<sr25519::Pair>("Alice//stash"),
+			account_id_from_seed::<sr25519::Pair>("Bob//stash"),
+		],
+	)
+}
+
+/// Helper function to build a genesis configuration
+pub fn testnet_genesis(root_key: AccountId, endowed_accounts: Vec<AccountId>) -> GenesisConfig {
+	GenesisConfig {
+		system: Some(SystemConfig {
+			code: WASM_BINARY.to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		balances: Some(BalancesConfig {
+			balances: endowed_accounts
+				.iter()
+				.cloned()
+				.map(|k| (k, 1 << 60))
+				.collect(),
+		}),
+		sudo: Some(SudoConfig { key: root_key }),
+	}
+}
