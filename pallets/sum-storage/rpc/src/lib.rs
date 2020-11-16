b3b4a1b42cb22ebc07cb35a094425ea75b467111
Joshy Orndorff <admin@joshyorndorff.com>
2020-11-16 11:46:58 -0500

    Introduce inner helper trait

--- pallets/sum-storage/rpc/src/lib.rs
+++ pallets/sum-storage/rpc/src/lib.rs
@@ -12,13 +12,27 @@ use sp_io::hashing::twox_128;
 use sp_storage::StorageKey;
 use codec::Decode;
 use sum_storage::SumStorageSchema;
-use sp_storage::StorageData;
+
+pub trait SumStorageApiHelper<Block: BlockT> {
+	fn get_sum(&self, at: BlockId<Block>) -> Result<u32>;
+}
 
 #[rpc]
 pub trait SumStorageApi<BlockHash> {
 	#[rpc(name = "sumStorage_getSum")]
 	fn get_sum(&self, at: Option<BlockHash>) -> Result<u32>;
 }
+
+// TODO Can I use a blanket imlementation like this? Seems like a mostly nice idea,
+// But I'm having some weird side effects. It seems like it is implementing the SumStorageApi for
+// stuff where I didn't explicitly implement the SumStorageApiHelper.
+// impl <BlockHash, T> SumStorageApi<BlockHash> for T
+// 	where T: SumStorageApiHelper<BlockHash> + Send + Sync + 'static
+// {
+// 	fn get_sum(&self, at: Option<BlockHash>) -> Result<u32> {
+// 		self.get_sum(at)
+// 	}
+// }
 
 /// A struct that implements the `SumStorageApi`.
 pub struct SumStorage<C, Block> {
@@ -36,24 +50,7 @@ impl<C, Block> SumStorage<C, Block> {
 	}
 }
 
-/// Error type of this RPC api.
-// pub enum Error {
-// 	/// The transaction was not decodable.
-// 	DecodeError,
-// 	/// The call to runtime failed.
-// 	RuntimeError,
-// }
-//
-// impl From<Error> for i64 {
-// 	fn from(e: Error) -> i64 {
-// 		match e {
-// 			Error::RuntimeError => 1,
-// 			Error::DecodeError => 2,
-// 		}
-// 	}
-// }
-
-impl<C, Block> SumStorageApi<<Block as BlockT>::Hash> for SumStorage<C, Block>
+impl<C, Block> SumStorageApiHelper<Block> for SumStorage<C, Block>
 where
 	Block: BlockT,
 	C: Send + Sync + 'static,
@@ -61,11 +58,8 @@ where
 	C: HeaderBackend<Block>,
 	C::Api: SumStorageRuntimeApi<Block>,
 {
-	fn get_sum(&self, at: Option<<Block as BlockT>::Hash>) -> Result<u32> {
+	fn get_sum(&self, at: BlockId<Block>) -> Result<u32> {
 		let api = self.client.runtime_api();
-		let at = BlockId::hash(at.unwrap_or_else(||
-			// If the block hash is not supplied assume the best block.
-			self.client.info().best_hash));
 
 		let runtime_api_result = api.get_sum(&at);
 		runtime_api_result.map_err(|e| RpcError {
@@ -101,7 +95,7 @@ impl<C, BE, Block> SumStorageOptimizedV1<C, BE, Block> {
 	}
 }
 
-impl<C, BE, Block> SumStorageApi<<Block as BlockT>::Hash> for SumStorageOptimizedV1<C, BE, Block>
+impl<C, BE, Block> SumStorageApiHelper<Block> for SumStorageOptimizedV1<C, BE, Block>
 where
 	Block: BlockT,
 	C: Send + Sync + 'static,
@@ -110,10 +104,7 @@ where
 	BE: Backend<Block> + 'static,
 	BE::State: StateBackend<BlakeTwo256>,
 {
-	fn get_sum(&self, at: Option<<Block as BlockT>::Hash>) -> Result<u32> {
-		let at = BlockId::hash(at.unwrap_or_else(||
-			// If the block hash is not supplied assume the best block.
-			self.client.info().best_hash));
+	fn get_sum(&self, at: BlockId<Block>) -> Result<u32> {
 
 		// Get Thing1 from storage
 		let thing1_encoded = self.client.storage(&at, &StorageKey(storage_prefix_build(b"SumStorage", b"Thing1")))
@@ -168,21 +159,30 @@ where
 
 /// A struct that implements the `SumStorageApi` by using hardcoded storage keys and the
 /// state backend.
-pub struct SumStorageOptimizedWithFallback<C, BE, Block> {
+pub struct SumStorageOptimizedWithFallback<C, BE, Block: BlockT> {
 	client: Arc<C>,
 	// Todo this needs to accept different types afaik. This attempt didn't work
-	// optimized: Box<dyn SumStorageApi<Block>>,
-	optimized: SumStorageOptimizedV1<C, BE, Block>,
+	optimized: Box<dyn SumStorageApiHelper<Block> + Send + Sync>,
+	// optimized: SumStorageOptimizedV1<C, BE, Block>,
 	fallback: SumStorage<C, Block>,
 	_marker: std::marker::PhantomData<BE>,
 }
 
-impl<C, BE, Block> SumStorageOptimizedWithFallback<C, BE, Block> {
-	/// Create new `SumStorage` instance with the given reference to the client.
+impl<C, BE, Block> SumStorageOptimizedWithFallback<C, BE, Block>
+	where
+		Block: BlockT,
+		C: Send + Sync + 'static,
+		C: StorageProvider<Block, BE>,
+		C: HeaderBackend<Block>,
+		C: ProvideRuntimeApi<Block>,
+		C::Api: SumStorageRuntimeApi<Block>,
+		BE: Backend<Block> + 'static,
+		BE::State: StateBackend<BlakeTwo256>,
+{
 	pub fn new(client: Arc<C>) -> Self {
 		Self {
 			client: client.clone(),
-			optimized: SumStorageOptimizedV1::new(client.clone()),
+			optimized: Box::new(SumStorageOptimizedV1::new(client.clone())),
 			fallback: SumStorage::new(client),
 			_marker: Default::default(),
 		}
@@ -218,12 +218,10 @@ where
 		// is V1. To make this generally useful we will need to match or lookup what handler to use based
 		// on the schema version stored on chain
 		if let SumStorageSchema::V1 = schema {
-			// Here we pass the same option for a block that we got in even thogu hwe have already processed that data once.
-			// This is another clue (along with the sized stuff) that there should be a different inner trait.
-			self.optimized.get_sum(maybe_at)
+			self.optimized.get_sum(at)
 		}
 		else {
-			self.fallback.get_sum(maybe_at)
+			SumStorageApiHelper::get_sum(&self.fallback, at)
 		}
 	}
 }
