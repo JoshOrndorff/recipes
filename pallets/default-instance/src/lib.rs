73ec31e23b64c46b0761eb77404d9c88fbf2b0c0
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-17 23:41:11 -0400

    default instance

--- pallets/default-instance/src/lib.rs
+++ pallets/default-instance/src/lib.rs
@@ -1,24 +1,25 @@
 //! An example instantiable pallet (with default instance)
+//! TODO combine this and last caller into a singe crate (see check membership for an example)
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{decl_event, decl_module, decl_storage, dispatch::DispatchResult};
-use frame_system::{self as system, ensure_signed};
+use frame_system::ensure_signed;
 
 // The pallet's configuration trait takes an instance as a type parameter. The instance type is
 // created by the `decl_storage!` macro below. Giving it a value of `DefaultInstance` allows us
 // to use the pallet in a runtime where only a single instance is desired without the extra syntax
 // that is otherwise needed to use instantiable pallets.
-pub trait Trait<I: Instance = DefaultInstance>: system::Trait {
+pub trait Config<I: Instance = DefaultInstance>: frame_system::Config {
 	// The ubiquitous event type's From bound needs updated to support the instance.
-	type Event: From<Event<Self, I>> + Into<<Self as system::Trait>::Event>;
+	type Event: From<Event<Self, I>> + Into<<Self as frame_system::Config>::Event>;
 }
 
 // It is necessary for instantiable pallets to call `decl_storage!` even if no storage items
 // are used so the instance type is created.
 decl_storage! {
 	// The storage trait also takes the Instance parameter
-	trait Store for Module<T: Trait<I>, I: Instance=DefaultInstance> as LastCaller {
+	trait Store for Module<T: Config<I>, I: Instance=DefaultInstance> as LastCaller {
 
 		// A single storage item that keeps track of
 		// which account last called its only dispatchable call.
@@ -30,7 +31,7 @@ decl_event!(
 	// The enum trait also takes the Instance as a parameter
 	pub enum Event<T, I = DefaultInstance>
 	where
-		AccountId = <T as system::Trait>::AccountId,
+		AccountId = <T as frame_system::Config>::AccountId,
 	{
 		Called(AccountId),
 	}
@@ -38,7 +39,7 @@ decl_event!(
 
 decl_module! {
 	// The `Module` struct also takes the instance parameter.
-	pub struct Module<T: Trait<I>, I: Instance = DefaultInstance> for enum Call where origin: T::Origin {
+	pub struct Module<T: Config<I>, I: Instance = DefaultInstance> for enum Call where origin: T::Origin {
 		fn deposit_event() = default;
 
 		/// The only dispatchable call, updates the single storage item,
