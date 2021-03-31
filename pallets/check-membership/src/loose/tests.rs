280b93c51b268720198e02e55a1855c70ac55c7c
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-31 14:30:09 -0400

    check membership tests

--- pallets/check-membership/src/loose/tests.rs
+++ pallets/check-membership/src/loose/tests.rs
@@ -1,85 +1,71 @@
-use crate::loose::*;
-use frame_support::{assert_noop, assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
-use frame_system as system;
+use crate::loose::{self as check_membership, Config, RawEvent, Error};
+use frame_support::{assert_noop, assert_ok, construct_runtime, parameter_types};
 use sp_core::H256;
 use sp_io::TestExternalities;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TestRuntime;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		VecSet: vec_set::{Module, Call, Storage, Event<T>},
+		CheckMembership: check_membership::{Module, Call, Event<T>},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: u32 = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
+	type SS58Prefix = ();
 }
 
-mod check_membership {
-	pub use crate::loose::Event;
-}
-
-impl_outer_event! {
-	pub enum TestEvent for TestRuntime {
-		vec_set<T>,
-		system<T>,
-		check_membership<T>,
-	}
-}
-
-impl vec_set::Trait for TestRuntime {
-	type Event = TestEvent;
+impl vec_set::Config for TestRuntime {
+	type Event = Event;
 }
 
 impl Config for TestRuntime {
-	type Event = TestEvent;
+	type Event = Event;
 	type MembershipSource = VecSet;
 }
-
-pub type System = system::Module<TestRuntime>;
-pub type VecSet = vec_set::Module<TestRuntime>;
-pub type CheckMembership = Module<TestRuntime>;
 
 struct ExternalityBuilder;
 
 impl ExternalityBuilder {
 	pub fn build() -> TestExternalities {
-		let storage = system::GenesisConfig::default()
+		let storage = frame_system::GenesisConfig::default()
 			.build_storage::<TestRuntime>()
 			.unwrap();
 		let mut ext = TestExternalities::from(storage);
@@ -95,7 +81,7 @@ fn members_can_call() {
 
 		assert_ok!(CheckMembership::check_membership(Origin::signed(1)));
 
-		let expected_event = TestEvent::check_membership(RawEvent::IsAMember(1));
+		let expected_event = Event::check_membership(RawEvent::IsAMember(1));
 
 		assert_eq!(System::events()[1].event, expected_event,);
 	})
