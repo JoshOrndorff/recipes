e174cc1bc3436c85e64ee7921e17942c41e103c7
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-31 15:21:52 -0400

    map set tests

--- pallets/map-set/src/tests.rs
+++ pallets/map-set/src/tests.rs
@@ -1,78 +1,65 @@
-use crate::*;
-use frame_support::{assert_noop, assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
-use frame_system as system;
+use crate::{self as check_membership, Config, RawEvent, Error};
+use frame_support::{assert_noop, assert_ok, construct_runtime, parameter_types};
 use sp_core::H256;
 use sp_io::TestExternalities;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TestRuntime;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		MapSet: map_set::{Module, Call, Storage, Event<T>},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: u32 = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-}
-
-mod vec_set {
-	pub use crate::Event;
-}
-
-impl_outer_event! {
-	pub enum TestEvent for TestRuntime {
-		vec_set<T>,
-		system<T>,
-	}
+	type SS58Prefix = ();
 }
 
 impl Config for TestRuntime {
-	type Event = TestEvent;
+	type Event = Event;
 }
-
-pub type System = system::Module<TestRuntime>;
-pub type MapSet = Module<TestRuntime>;
 
 struct ExternalityBuilder;
 
 impl ExternalityBuilder {
 	pub fn build() -> TestExternalities {
-		let storage = system::GenesisConfig::default()
+		let storage = frame_system::GenesisConfig::default()
 			.build_storage::<TestRuntime>()
 			.unwrap();
 		let mut ext = TestExternalities::from(storage);
@@ -86,7 +73,7 @@ fn add_member_works() {
 	ExternalityBuilder::build().execute_with(|| {
 		assert_ok!(MapSet::add_member(Origin::signed(1)));
 
-		let expected_event = TestEvent::vec_set(RawEvent::MemberAdded(1));
+		let expected_event = Event::map_set(RawEvent::MemberAdded(1));
 
 		assert_eq!(
 			System::events()[0].event,
@@ -132,7 +119,7 @@ fn remove_member_works() {
 		assert_ok!(MapSet::remove_member(Origin::signed(1)));
 
 		// check correct event emission
-		let expected_event = TestEvent::vec_set(RawEvent::MemberRemoved(1));
+		let expected_event = Event::map_set(RawEvent::MemberRemoved(1));
 
 		assert_eq!(
 			System::events()[1].event,
