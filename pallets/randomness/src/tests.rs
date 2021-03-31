152dedbaa43efb0badb9152f9582aa26782f7244
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-31 15:36:36 -0400

    Try and fail to get tests working for randomness recipe

--- pallets/randomness/src/tests.rs
+++ pallets/randomness/src/tests.rs
@@ -1,80 +1,70 @@
-use crate::{Event, Module, Trait};
-use frame_support::{assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
-use frame_system::{self as system, EventRecord, Phase};
+use crate::{self as randomness, Event as PalletEvent, Config};
+use frame_support::{assert_ok, construct_runtime, parameter_types};
 use sp_core::H256;
 use sp_io;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TestRuntime;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		CollectiveFlip: pallet_randomness_collective_flip::{Module, Call, Storage},
+		Randomness: randomness::{Module, Call, Storage, Event},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: u32 = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-}
-
-mod randomness {
-	pub use crate::Event;
-}
-
-impl_outer_event! {
-	pub enum TestEvent for TestRuntime {
-		randomness,
-		system<T>,
-	}
+	type SS58Prefix = ();
 }
 
 impl Config for TestRuntime {
-	type Event = TestEvent;
+	type Event = Event;
 	type RandomnessSource = CollectiveFlip;
 }
 
-pub type System = system::Module<TestRuntime>;
-pub type Randomness = Module<TestRuntime>;
-pub type CollectiveFlip = pallet_randomness_collective_flip::Module<TestRuntime>;
+// There is no config trait...
+// impl pallet_randomness_collective_flip::Config for TestRuntime {}
 
 struct ExternalityBuilder;
 
 impl ExternalityBuilder {
 	pub fn build() -> sp_io::TestExternalities {
-		let storage = system::GenesisConfig::default()
+		let storage = frame_system::GenesisConfig::default()
 			.build_storage::<TestRuntime>()
 			.unwrap();
 		let mut ext = sp_io::TestExternalities::from(storage);
@@ -89,15 +79,11 @@ fn generate_works() {
 		assert_ok!(Randomness::consume_randomness(Origin::signed(1)));
 
 		// Check for the event
+		let expected_event = Event::randomness(PalletEvent::RandomnessConsumed(H256::zero(), H256::zero()));
+
 		assert_eq!(
-			System::events(),
-			vec![EventRecord {
-				phase: Phase::Initialization,
-				event: TestEvent::randomness(
-					Event::RandomnessConsumed(H256::zero(), H256::zero(),)
-				),
-				topics: vec![],
-			}]
+			System::events()[0].event,
+			expected_event
 		);
 	})
 }
