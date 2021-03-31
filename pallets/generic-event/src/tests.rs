18a1aca695039e61f442b2f2a4e1ab8a9bac1e1f
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-31 15:15:39 -0400

    generic-event tests

--- pallets/generic-event/src/tests.rs
+++ pallets/generic-event/src/tests.rs
@@ -1,78 +1,65 @@
-use crate::{Module, RawEvent, Trait};
-use frame_support::{assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
-use frame_system as system;
+use crate::{self as generic_event, Config, RawEvent};
+use frame_support::{assert_ok, construct_runtime, parameter_types};
 use sp_core::H256;
 use sp_io::TestExternalities;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TestRuntime;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		GenericEvent: generic_event::{Module, Call, Event<T>},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: u32 = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-}
-
-mod generic_event {
-	pub use crate::Event;
-}
-
-impl_outer_event! {
-	pub enum TestEvent for TestRuntime {
-		generic_event<T>,
-		system<T>,
-	}
+	type SS58Prefix = ();
 }
 
 impl Config for TestRuntime {
-	type Event = TestEvent;
+	type Event = Event;
 }
-
-pub type System = system::Module<TestRuntime>;
-pub type GenericEvent = Module<TestRuntime>;
 
 struct ExternalityBuilder;
 
 impl ExternalityBuilder {
 	pub fn build() -> TestExternalities {
-		let storage = system::GenesisConfig::default()
+		let storage = frame_system::GenesisConfig::default()
 			.build_storage::<TestRuntime>()
 			.unwrap();
 		let mut ext = TestExternalities::from(storage);
@@ -87,7 +74,7 @@ fn test() {
 		assert_ok!(GenericEvent::do_something(Origin::signed(1), 32));
 
 		// construct event that should be emitted in the method call directly above
-		let expected_event = TestEvent::generic_event(RawEvent::EmitInput(1, 32));
+		let expected_event = Event::generic_event(RawEvent::EmitInput(1, 32));
 
 		// iterate through array of `EventRecord`s
 		assert_eq!(
