bd20970fe0e8de4bf71237838429c494676b15dd
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-31 14:43:39 -0400

    double map tests

--- pallets/double-map/src/tests.rs
+++ pallets/double-map/src/tests.rs
@@ -1,78 +1,65 @@
-use super::*;
-use frame_support::{assert_noop, assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
-use frame_system as system;
+use crate::{self as double_map, Config, RawEvent, GroupMembership, MemberScore};
+use frame_support::{assert_noop, assert_ok, construct_runtime, parameter_types, storage::{StorageMap,StorageDoubleMap}};
 use sp_core::H256;
 use sp_io::TestExternalities;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TestRuntime;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		DoubleMap: double_map::{Module, Call, Storage, Event<T>},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: u32 = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
 impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = TestEvent;
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-}
-
-mod double_map {
-	pub use crate::Event;
-}
-
-impl_outer_event! {
-	pub enum TestEvent for TestRuntime {
-		double_map<T>,
-		system<T>,
-	}
+	type SS58Prefix = ();
 }
 
 impl Config for TestRuntime {
-	type Event = TestEvent;
+	type Event = Event;
 }
-
-pub type System = system::Module<TestRuntime>;
-pub type DoubleMap = Module<TestRuntime>;
 
 struct ExternalityBuilder;
 
 impl ExternalityBuilder {
 	pub fn build() -> TestExternalities {
-		let storage = system::GenesisConfig::default()
+		let storage = frame_system::GenesisConfig::default()
 			.build_storage::<TestRuntime>()
 			.unwrap();
 		let mut ext = TestExternalities::from(storage);
@@ -92,7 +79,7 @@ fn join_all_members_works() {
 		);
 
 		// correct event emission
-		let expected_event = TestEvent::double_map(RawEvent::NewMember(1));
+		let expected_event = Event::double_map(RawEvent::NewMember(1));
 
 		assert_eq!(
 			System::events()[0].event,
@@ -116,7 +103,7 @@ fn group_join_works() {
 		assert_ok!(DoubleMap::join_a_group(Origin::signed(1), 3, 5));
 
 		// correct event emission
-		let expected_event = TestEvent::double_map(RawEvent::MemberJoinsGroup(1, 3, 5));
+		let expected_event = Event::double_map(RawEvent::MemberJoinsGroup(1, 3, 5));
 
 		assert_eq!(
 			System::events()[1].event,
@@ -140,7 +127,7 @@ fn remove_member_works() {
 		assert_ok!(DoubleMap::remove_member(Origin::signed(1)));
 
 		// check: correct event emitted
-		let expected_event = TestEvent::double_map(RawEvent::RemoveMember(1));
+		let expected_event = Event::double_map(RawEvent::RemoveMember(1));
 
 		assert_eq!(
 			System::events()[2].event,
@@ -176,7 +163,7 @@ fn remove_group_score_works() {
 		assert_ok!(DoubleMap::remove_group_score(Origin::signed(1), 3));
 
 		// correct event emitted
-		let expected_event = TestEvent::double_map(RawEvent::RemoveGroup(3));
+		let expected_event = Event::double_map(RawEvent::RemoveGroup(3));
 
 		assert_eq!(
 			System::events()[6].event,
