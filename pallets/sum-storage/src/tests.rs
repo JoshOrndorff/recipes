625d3b263b4a761bd1ee85981b152c9c67efbdb1
Joshy Orndorff <admin@joshyorndorff.com>
2021-04-01 11:44:04 -0400

    sum storage tests

--- pallets/sum-storage/src/tests.rs
+++ pallets/sum-storage/src/tests.rs
@@ -1,65 +1,65 @@
-use super::*;
+use crate::{self as sum_storage, Config};
 
-use frame_support::{assert_ok, impl_outer_origin, parameter_types, weights::Weight};
+use frame_support::{assert_ok, construct_runtime, parameter_types};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
 };
 
-impl_outer_origin! {
-	pub enum Origin for Test {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
-// For testing the module, we construct most of a mock runtime. This means
-// first constructing a configuration type (`Test`) which `impl`s each of the
-// configuration traits of modules we want to use.
-#[derive(Clone, Eq, PartialEq)]
-pub struct Test;
+construct_runtime!(
+	pub enum TestRuntime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		SumStorage: sum_storage::{Module, Call, Storage, Event},
+	}
+);
+
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: Weight = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
 }
-impl frame_system::Config for Test {
+impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
 	type Origin = Origin;
-	type Call = ();
 	type Index = u64;
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = ();
+	type Event = Event;
 	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
+	type SS58Prefix = ();
 }
-impl Trait for Test {
-	type Event = ();
+
+impl Config for TestRuntime {
+	type Event = Event;
 }
-type TemplateModule = Module<Test>;
 
 // This function basically just builds a genesis storage key/value store according to
 // our desired mockup.
 fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default()
-		.build_storage::<Test>()
+	frame_system::GenesisConfig::default()
+		.build_storage::<TestRuntime>()
 		.unwrap()
 		.into()
 }
@@ -67,31 +67,31 @@ fn new_test_ext() -> sp_io::TestExternalities {
 #[test]
 fn default_sum_zero() {
 	new_test_ext().execute_with(|| {
-		assert_eq!(TemplateModule::get_sum(), 0);
+		assert_eq!(SumStorage::get_sum(), 0);
 	});
 }
 
 #[test]
 fn sums_thing_one() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(TemplateModule::set_thing_1(Origin::signed(1), 42));
-		assert_eq!(TemplateModule::get_sum(), 42);
+		assert_ok!(SumStorage::set_thing_1(Origin::signed(1), 42));
+		assert_eq!(SumStorage::get_sum(), 42);
 	});
 }
 
 #[test]
 fn sums_thing_two() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(TemplateModule::set_thing_2(Origin::signed(1), 42));
-		assert_eq!(TemplateModule::get_sum(), 42);
+		assert_ok!(SumStorage::set_thing_2(Origin::signed(1), 42));
+		assert_eq!(SumStorage::get_sum(), 42);
 	});
 }
 
 #[test]
 fn sums_both_values() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(TemplateModule::set_thing_1(Origin::signed(1), 42));
-		assert_ok!(TemplateModule::set_thing_2(Origin::signed(1), 43));
-		assert_eq!(TemplateModule::get_sum(), 85);
+		assert_ok!(SumStorage::set_thing_1(Origin::signed(1), 42));
+		assert_ok!(SumStorage::set_thing_2(Origin::signed(1), 43));
+		assert_eq!(SumStorage::get_sum(), 85);
 	});
 }
