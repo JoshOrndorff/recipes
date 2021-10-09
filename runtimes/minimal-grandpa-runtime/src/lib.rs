fe392a00dc947d4cbe143a21c7f678c3a813db03
Elio Prifti <elioprifti@gmail.com>
2021-10-09 21:07:18 +0200

    fix clippy errors

--- runtimes/minimal-grandpa-runtime/src/lib.rs
+++ runtimes/minimal-grandpa-runtime/src/lib.rs
@@ -5,7 +5,7 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 // #![allow(clippy::large_enum_variant)]
-
+#![allow(clippy::from_over_into)]
 // Make the WASM binary available.
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
